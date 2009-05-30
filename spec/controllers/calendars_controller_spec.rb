775326a015b87e96b438199dfe5720add6fbac8e
David Cato <git@crunchyfrog.net>
2009-05-29 19:55:31 -0700

    Fix REs in specs
    The specs were never updated after implementing the RedCloth bug hack
    which caused the tests for calendar contents to fail because of the
    added newlines.

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -8,7 +8,7 @@ describe CalendarsController do
     # DANGER: The returned HTML is JSON-encoded. All the backslashes in the
     # substitution really are required to get the correct matcher. Perhaps
     # this is overkill and we don't need to bother checking the return string?
-    re = %Q{<th .*?class=\\"monthName\\".*?>#{Date::MONTHNAMES[Date.today.month]}</th>}
+    re = %Q{<th .*?class=\\"monthName\\".*?>#{Date::MONTHNAMES[Date.today.month]}\\n?</th>}
     expected = %r{#{json_escape(re).gsub('\\','\\\\\\').sub('\\\\monthName\\\\','\\\\\\\\"monthName\\\\\\\\"')}}
 
     xhr :get, :show, :year => Date.today.year, :month => Date.today.month
