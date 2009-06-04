152e17a2905bcbdd5798d7645b88bd83691594de
David Cato <git@crunchyfrog.net>
2009-06-04 01:31:51 -0700

    More workarounds for RedCloth quirks

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -37,17 +37,17 @@ module CalendarsHelper
       block << %Q{<div id="#{div_id}">}
       block << calendar(calendar_options) do |d|
         if days_with_events.has_key?(d)
-          event_list = %Q{<div id="events-#{d.jd}" class="calendar-data"><dl class="events">}
+          event_list = %Q{<div id="events-#{d.jd}" class="calendar-data">\n<dl class="events">\n}
           eod = d + 1.day - 1.second
           # Sort events by start time, with full-day events being placed
           # at the end, followed by a secondary sort on the event name.
           days_with_events[d].sort_by{|e| [e.start_time||eod,e.name]}.each do |event|
-            event_list << %Q{<dt>#{h event.name}</dt>}
-            event_list << %Q{<dd class="location">#{h event.location}</dd>} if event.location
-            event_list << %Q{<dd class="time">#{h event.time(:format => '%l:%M %p')}</dd>} if event.time
-            event_list << %Q{<dd class="description">#{h(event.description) || '&nbsp;'}</dd>}
+            event_list << %Q{<dt>#{h event.name}</dt>\n}
+            event_list << %Q{<dd class="location">#{h event.location}</dd>\n} if event.location
+            event_list << %Q{<dd class="time">#{h event.time(:format => '%l:%M %p')}</dd>\n} if event.time
+            event_list << %Q{<dd class="description">#{h(event.description) || '&nbsp;'}</dd>\n}
           end
-          event_list << '</dl><p class="footer">Click date for more details</p></div>'
+          event_list << %Q{</dl>\n<p class="footer">Click date for more details</p>\n</div>\n}
           events << event_list
 
           [ %Q{<a href="#{events_path(:year => this_month.year, :month => this_month.month, :day => d.mday)}">#{d.mday}</a>}, { :class => 'eventDay', :id => "day-#{d.jd}" } ]
@@ -56,7 +56,7 @@ module CalendarsHelper
       end.gsub(/(<\/?(table|thead|tbody|tfoot|tr|th|td)[^>]*?>)/, "\n"+'\1')
       block << "\n"
       block << events.join
-      block << "\n</div>"
+      block << '</div>'
       block << %Q{\n<script type="text/javascript">makeToolTips();</script>\n}
     end
   end
