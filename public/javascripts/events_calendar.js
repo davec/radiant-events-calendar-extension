2b1e801fb5252555be04c5b9748d87320cede3f3
David Cato <git@crunchyfrog.net>
2009-06-19 21:48:26 -0700

    Improve tooltip size calculations
    This change also constrains the tooltip to fit within the viewport

--- public/javascripts/events_calendar.js
+++ public/javascripts/events_calendar.js
@@ -21,26 +21,34 @@ var ToolTip = Class.create({
       // Copy the contents of the tooltip to the active-tooltip div
       ActiveToolTip.update(this.element.innerHTML);
       ActiveToolTip.id = 'tooltip-data-'+this.element.id;
-
-      ActiveToolTip.style.width  = this.element.offsetWidth  + 'px';
-      ActiveToolTip.style.height = this.element.offsetHeight + 'px';
-
-      // Calculate the tooltip position
-      var viewport_offset = document.viewport.getScrollOffsets();
-      var parent_offset = this.parent.cumulativeOffset();
-      var y = Math.max(Math.abs(this.offset_y) + viewport_offset.top,
-                       parent_offset.top - this.offset_y - ActiveToolTip.getHeight() + this.parent.getHeight());
-      var x = parent_offset.left - this.offset_x - ActiveToolTip.getWidth();
-      // If the tooltip doesn't fit on the left, move it to the right
-      if (x < 0) {
-        x = parent_offset.left + this.offset_x + this.parent.getWidth();
-      }
-
-      // Set the tooltip position and make it visible
-      ActiveToolTip.style.top  = y + 'px';
-      ActiveToolTip.style.left = x + 'px';
     }
+
+    // Calculate the tooltip size (and prevent overflowing the viewport)
+    var max_tooltip_width = document.viewport.getWidth() - (parseInt(this.element.getStyle('padding-left')) +
+                                                            parseInt(this.element.getStyle('padding-right')) +
+                                                            2 * Math.abs(this.offset_x));
+    var max_tooltip_height = document.viewport.getHeight() - (parseInt(this.element.getStyle('padding-top')) +
+                                                              parseInt(this.element.getStyle('padding-bottom')) +
+                                                              2 * Math.abs(this.offset_y));
+    ActiveToolTip.style.width  = Math.min(parseInt(this.element.getStyle('width')), max_tooltip_width) + 'px';
+    ActiveToolTip.style.height = Math.min(parseInt(this.element.getStyle('height')), max_tooltip_height) + 'px';
+
+    // Calculate the tooltip position
+    var viewport_offset = document.viewport.getScrollOffsets();
+    var parent_offset = this.parent.cumulativeOffset();
+    var y = Math.max(Math.abs(this.offset_y) + viewport_offset.top,
+                     parent_offset.top - this.offset_y - ActiveToolTip.getHeight() + this.parent.getHeight());
+    var x = parent_offset.left - this.offset_x - ActiveToolTip.getWidth();
+    // If the tooltip doesn't fit on the left, move it to the right
+    if (x < 0) {
+      x = parent_offset.left + this.offset_x + this.parent.getWidth();
+    }
+
+    // Set the tooltip position and make it visible
+    ActiveToolTip.style.top  = y + 'px';
+    ActiveToolTip.style.left = x + 'px';
     ActiveToolTip.style.visibility = 'visible';
+    ActiveToolTip.style.overflow = 'hidden';
   }
 });
 
