edb8d1eb974a556cb00ad2e3621fdf5c4e942a12
David Cato <git@crunchyfrog.net>
2009-08-13 13:14:56 -0700

    Set start_time to match date
    Two events used Date.today.year for the date but hard-coded year 2009
    for the start_time.

--- spec/datasets/events_dataset.rb
+++ spec/datasets/events_dataset.rb
@@ -6,9 +6,9 @@ class EventsDataset < Dataset::Base
     create_event("Today's late night event", Date.today, :description => "Another event happening today.")
     create_event("Yesterday's event", Date.yesterday, :description => "An event that happened yesterday.")
     create_event("Tomorrow's event", Date.tomorrow, :description => "An event happening tomorrow.")
-    create_event("New Year's Day", Date.civil(Date.today.year,1,1), :description => "New Year's Party", :location => "Swanky Hotel", :start_time => Time.local(2009,1,1,0,0,0))
+    create_event("New Year's Day", Date.civil(Date.today.year,1,1), :description => "New Year's Party", :location => "Swanky Hotel", :start_time => Time.local(Date.today.year,1,1,0,0,0))
     create_event("Jan 2009 event", Date.civil(2009,1,15), :start_time => Time.local(2009,1,15,8,0,0), :end_time => Time.local(2009,1,15,17,0,0))
-    create_event("Independence Day", Date.civil(Date.today.year,7,4), :description => "Fireworks", :location => "Down by the river", :category => "Holidays", :start_time => Time.local(2009,7,4,20,0,0))
+    create_event("Independence Day", Date.civil(Date.today.year,7,4), :description => "Fireworks", :location => "Down by the river", :category => "Holidays", :start_time => Time.local(Date.today.year,7,4,20,0,0))
   end
 
   helpers do
