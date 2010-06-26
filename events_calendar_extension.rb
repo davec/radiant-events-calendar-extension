dbf0c5b6400b8bb9186b3176c2e153ca955bb659
David Cato <git@crunchyfrog.net>
2010-06-25 21:00:05 -0700

    Move route defs to config/routes.rb

--- events_calendar_extension.rb
+++ events_calendar_extension.rb
@@ -6,23 +6,6 @@ class EventsCalendarExtension < Radiant::Extension
   description "Adds a calendar of events to your Radiant site."
   url "http://github.com/davec/radiant-events-calendar-extension"
   
-  define_routes do |map|
-    map.namespace :admin, :member => { :remove => :get, :copy => :get } do |admin|
-      admin.resources :events, :collection => { :auto_complete_for_event_category => :get }
-    end
-    map.events '/events/:year/:month/:day', :controller => 'events',
-                                            :action => 'show',
-                                            :year => /20\d\d/,
-                                            :month => /(0?[1-9])|(1[0-2])/,
-                                            :day => /(0?[1-9])|([12]\d)|(3[01])/,
-                                            :conditions => { :method => :get }
-    map.calendar '/calendar/:year/:month',  :controller => 'calendars',
-                                            :action => 'show',
-                                            :year => /20\d\d/,
-                                            :month => /(0?[1-9])|(1[0-2])/,
-                                            :conditions => { :method => :get }
-  end
-  
   def activate
     admin.tabs.add "Events", "/admin/events", :after => "Layouts"
     Page.send :include, EventsCalendarTags
