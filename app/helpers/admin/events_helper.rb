a89d1eea167a247b07c7b190a350eed634e69b04
David Cato <git@crunchyfrog.net>
2010-06-29 11:03:19 -0700

    Update filter reference display

--- app/helpers/admin/events_helper.rb
+++ app/helpers/admin/events_helper.rb
@@ -1,2 +1,24 @@
 module Admin::EventsHelper
+  def event_edit_javascripts
+    <<-CODE
+      var lastFilter = '#{@event.filter_id}';
+      var filterWindows = {};
+      function loadFilterReference() {
+        var filter = $F('event_filter_id');
+        if (filter != "") {
+          if (!filterWindows[filter]) filterWindows[filter] = new Popup.AjaxWindow("#{admin_reference_path('filters')}?filter_name="+encodeURIComponent(filter), {reload: false});
+          var window = filterWindows[filter];
+          if(lastFilter != filter) {
+            window.show();
+          } else {
+            window.toggle();
+          }
+          lastFilter = filter;
+        } else {
+          alert('No documentation for filter.');
+        }
+        return false;
+      }
+    CODE
+  end
 end
