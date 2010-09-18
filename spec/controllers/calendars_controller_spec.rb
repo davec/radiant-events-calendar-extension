3bfb628166d6d01c7c15eab5e823955fbce84529
David Cato <git@crunchyfrog.net>
2010-09-18 14:32:00 -0700

    Make the public-facing calendar work with jQuery
    Adds an alternate JS file named events_calendar_jquery.js that replaces
    events_calendar.js when you're using jQuery in your project.

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -5,22 +5,21 @@ describe CalendarsController do
   dataset :events
 
   it 'should return a calendar for the given month with an AJAX call' do
-    # DANGER: The returned HTML is JSON-encoded. All the backslashes in the
-    # substitution really are required to get the correct matcher. Perhaps
-    # this is overkill and we don't need to bother checking the return string?
-    re = %Q{<th .*?class=\\"monthName\\".*?>#{Date::MONTHNAMES[Date.today.month]}\\n?</th>}
-    expected = %r{#{json_escape(re).gsub('\\','\\\\\\').sub('\\\\monthName\\\\','\\\\\\\\"monthName\\\\\\\\"')}}
+    date = Date.today
+    expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[date.month]}\n?</th>}
 
-    xhr :get, :show, :year => Date.today.year, :month => Date.today.month
+    xhr :get, :show, :year => date.year, :month => date.month
     response.should be_success
-    response.should have_rjs(:replace, 'events-calendar')
     response.should have_text(expected)
   end
 
   it 'should return a calendar for the given month with a non-AJAX call' do
-    next_month = Date.today + 1.month
-    get :show, :year => next_month.year, :month => next_month.month
+    date = Date.today + 1.month
+    expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[date.month]}\n?</th>}
+
+    get :show, :year => date.year, :month => date.month
     response.should be_success
+    response.should have_text(expected)
   end
 
 end
