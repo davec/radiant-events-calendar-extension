39d0e80f29f92e9e24ad06160d3e2a96b57ffa74
David Cato <git@crunchyfrog.net>
2010-10-24 20:37:28 -0700

    Fix end time adjustment for events on last day of month

--- spec/controllers/admin/events_controller_spec.rb
+++ spec/controllers/admin/events_controller_spec.rb
@@ -18,22 +18,24 @@ describe Admin::EventsController do
         :timezone         => "UTC"
       }
       response.should redirect_to(admin_events_url)
-      Event.find_by_name(event_name).should_not be_nil
+      event = Event.find_by_name(event_name)
+      event.should_not be_nil
+      event.start_time.to_date.should == event.end_time.to_date
     end
 
     it "should advance an end time to the next day" do
       event_name = "Spanning midnight"
       post :create, :event => {
         :name             => event_name,
-        :date             => Date.today.to_s(:long),
+        :date             => Date.today.end_of_year.to_s(:long),
         :'start_time(5i)' => "19:00:00",
         :'end_time(5i)'   => "01:00:00",
         :timezone         => "UTC"
       }
       event = Event.find_by_name(event_name)
       event.should_not be_nil
-      event.start_time.to_date.should == Date.today
-      event.end_time.to_date.should == Date.tomorrow
+      event.start_time.to_date.should == Date.today.end_of_year
+      event.end_time.to_date.should == (Date.today >> 12).beginning_of_year
     end
   end
 end
