b882ae090b780ae018d87224a267a7e4e18f0565
David Cato <git@crunchyfrog.net>
2010-10-24 20:15:14 -0700

    Allow events to end after midnight
    If end_time < start_time, advance end_time by 1 day

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -34,14 +34,12 @@ class Admin::EventsController < Admin::ResourceController
     end
 
     def adjust_times
-      start_time = parse_time(params[:event][:'start_time(5i)'])
-      end_time = parse_time(params[:event][:'end_time(5i)'])
-      params[:event].delete(:'start_time(5i)')
-      params[:event].delete(:'end_time(5i)')
+      start_time = parse_time(params[:event].delete('start_time(5i)'))
+      end_time = parse_time(params[:event].delete('end_time(5i)'))
 
       date = Date.parse(params[:event][:date])
       params[:event][:start_time] = start_time.blank? ? start_time : start_time.change(:year => date.year, :month => date.month, :day => date.day)
-      params[:event][:end_time] = end_time.blank? ? end_time : end_time.change(:year => date.year, :month => date.month, :day => date.day)
+      params[:event][:end_time] = end_time.blank? ? end_time : end_time.change(:year => date.year, :month => date.month, :day => date.day + (start_time < end_time ? 0 : 1))
     end
 
   private
