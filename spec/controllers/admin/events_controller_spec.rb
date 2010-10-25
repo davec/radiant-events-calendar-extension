9231d65d9de9e0cac914defbd60a62d28167ce6c
David Cato <git@crunchyfrog.net>
2010-10-24 20:15:14 -0700

    Allow events to end after midnight
    If end_time < start_time, advance end_time by 1 day

--- spec/controllers/admin/events_controller_spec.rb
+++ spec/controllers/admin/events_controller_spec.rb
@@ -1,10 +1,39 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::EventsController do
+  dataset :users
 
-  ##Delete this example and add some real ones
-  #it "should use Admin::EventsController" do
-  #  controller.should be_an_instance_of(Admin::EventsController)
-  #end
+  before(:each) do
+    login_as :admin
+  end
 
+  describe "create" do
+    it "should be successful" do
+      event_name = "Event 1"
+      post :create, :event => {
+        :name             => event_name,
+        :date             => Date.today.to_s(:long),
+        :'start_time(5i)' => "09:00:00",
+        :'end_time(5i)'   => "17:00:00",
+        :timezone         => "UTC"
+      }
+      response.should redirect_to(admin_events_url)
+      Event.find_by_name(event_name).should_not be_nil
+    end
+
+    it "should advance an end time to the next day" do
+      event_name = "Spanning midnight"
+      post :create, :event => {
+        :name             => event_name,
+        :date             => Date.today.to_s(:long),
+        :'start_time(5i)' => "19:00:00",
+        :'end_time(5i)'   => "01:00:00",
+        :timezone         => "UTC"
+      }
+      event = Event.find_by_name(event_name)
+      event.should_not be_nil
+      event.start_time.to_date.should == Date.today
+      event.end_time.to_date.should == Date.tomorrow
+    end
+  end
 end
