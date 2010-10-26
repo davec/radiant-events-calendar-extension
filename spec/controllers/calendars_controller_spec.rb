c96095f843938b5d447c124ef59d87a32249c738
David Cato <git@crunchyfrog.net>
2010-10-26 13:41:55 -0700

    Re-order response targets
    Check for html before js

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -4,22 +4,35 @@ describe CalendarsController do
   integrate_views
   dataset :events
 
-  it 'should return a calendar for the given month with an AJAX call' do
-    date = Date.today
-    expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[date.month]}\n?</th>}
-
-    xhr :get, :show, :year => date.year, :month => date.month
-    response.should be_success
-    response.should have_text(expected)
+  before(:each) do
+    @today = Date.today
+    request.env['HTTP_REFERER'] = 'http://test.host/'
   end
 
-  it 'should return a calendar for the given month with a non-AJAX call' do
-    date = Date.today + 1.month
-    expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[date.month]}\n?</th>}
-
-    get :show, :year => date.year, :month => date.month
-    response.should be_success
-    response.should have_text(expected)
+  describe "routing" do
+    it 'should route to the show action' do
+      params_from(:get, "/calendar/#{@today.year}/#{@today.month}").should == { :controller => 'calendars', :action => 'show', :year => @today.year.to_s, :month => @today.month.to_s }
+    end
   end
 
+  describe "via AJAX" do
+    it 'should return a calendar for the given month' do
+      expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[@today.month]}\n?</th>}
+
+      xhr :get, :show, :year => @today.year, :month => @today.month
+      response.should be_success
+      response.should have_text(expected)
+    end
+  end
+
+  describe "via non-AJAX" do
+    it 'should redirect to the referring page, setting session params to generate a calendar for the given month' do
+      date = Date.today + 1.month
+      expected = %r{<th .*?class="monthName".*?>#{Date::MONTHNAMES[date.month]}\n?</th>}
+
+      get :show, :year => date.year, :month => date.month
+      response.should redirect_to(request.referer)
+      session[:calendar_view].should == { :year => date.year.to_s, :month => date.month.to_s }
+    end
+  end
 end
