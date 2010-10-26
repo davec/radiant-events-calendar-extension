cd9f6ec93d0996db26e2db8362b3f87adc39d3f2
David Cato <git@crunchyfrog.net>
2010-10-26 13:54:59 -0700

    Version bump

--- events_calendar_extension.rb
+++ events_calendar_extension.rb
@@ -1,5 +1,5 @@
 class EventsCalendarExtension < Radiant::Extension
-  version "0.9.2"  # Compatible with Radiant 0.9
+  version "0.9.3"  # Compatible with Radiant 0.9
   description "Adds a calendar of events to your Radiant site."
   url "http://github.com/davec/radiant-events-calendar-extension"
   
