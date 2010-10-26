969ce44e2a63e66c28c28d2dbb0805bc533d4fd6
David Cato <git@crunchyfrog.net>
2010-10-26 13:41:55 -0700

    Re-order response targets
    Check for html before js

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,16 +5,16 @@ class CalendarsController < ApplicationController
 
   def show
     respond_to do |format|
+      format.html {
+        session[:calendar_view] = { :year => params[:year], :month => params[:month] }
+        redirect_to(:back)
+      }
       format.js {
         if calendar = make_calendar(Date.civil(params[:year].to_i, params[:month].to_i)) rescue nil
           response.headers['Content-Type'] = 'text/html'
           render :text => calendar and return
         end
         render :text => 'Calendar FAIL', :status => 500
-      }
-      format.html {
-        session[:calendar_view] = { :year => params[:year], :month => params[:month] }
-        redirect_to(:back)
       }
     end
   end
